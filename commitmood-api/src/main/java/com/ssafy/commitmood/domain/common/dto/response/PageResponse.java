9ade5e753a61bcded69d3c2bb6511cdaa06c9f66
yeseong <yeseong31@naver.com>
2025-12-03 16:14:53 +0900

    ✨ feat(common): PageResponse 추가
    - 공용 페이징 응답 DTO 추가

---
+++ commitmood-api/src/main/java/com/ssafy/commitmood/domain/common/dto/response/PageResponse.java
@@ -0,0 +1,33 @@
+package com.ssafy.commitmood.domain.common.dto.response;
+
+import java.util.List;
+
+public record PageResponse<T>(
+        List<T> content,
+        int page,
+        int size,
+        long totalCount,
+        boolean hasNext,
+        int totalPages
+) {
+
+    public static <T> PageResponse<T> of(
+            List<T> content,
+            int page,
+            int size,
+            long totalCount
+    ) {
+
+        int totalPages = (int) Math.ceil((double) totalCount / size);
+        boolean hasNextPage = totalPages > page;
+
+        return new PageResponse<T>(
+                content,
+                page,
+                size,
+                totalCount,
+                hasNextPage,
+                totalPages
+        );
+    }
+}
