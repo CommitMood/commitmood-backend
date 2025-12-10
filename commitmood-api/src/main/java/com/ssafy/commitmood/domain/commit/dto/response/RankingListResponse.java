7672f933979702f879cf9a7705a222e9660350bb
gimseonghwan <swkim12345@naver.com>
2025-12-10 17:45:26 +0900

    📝docs(Ranking): response, controller swagger 추가

--- commitmood-api/src/main/java/com/ssafy/commitmood/domain/commit/dto/response/RankingListResponse.java
+++ commitmood-api/src/main/java/com/ssafy/commitmood/domain/commit/dto/response/RankingListResponse.java
@@ -1,13 +1,22 @@
 package com.ssafy.commitmood.domain.commit.dto.response;
 
 import com.ssafy.commitmood.domain.commit.dto.UserRankingDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
+@Schema(description = "사용자 랭킹 목록 응답")
 public record RankingListResponse (
+        @Schema(description = "사용자 랭킹 목록")
         List<UserRankingDto> rankings,
+
+        @Schema(description = "현재 페이지", example = "1")
         int page,
+
+        @Schema(description = "페이지당 항목 수", example = "30")
         int perPage,
+
+        @Schema(description = "전체 항목 수", example = "30")
         int totalCount
 ) {
     public static RankingListResponse of(
