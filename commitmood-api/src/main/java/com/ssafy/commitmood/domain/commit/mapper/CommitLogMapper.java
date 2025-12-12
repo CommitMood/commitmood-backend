555cc91b48f78959e97a1352db4db9f4f69b34a8
yeseong <yeseong31@naver.com>
2025-12-12 10:29:47 +0900

    📝 docs(commit): CommitLogMapper에 주석 추가 및 문서화 정비

--- commitmood-api/src/main/java/com/ssafy/commitmood/domain/commit/mapper/CommitLogMapper.java
+++ commitmood-api/src/main/java/com/ssafy/commitmood/domain/commit/mapper/CommitLogMapper.java
@@ -8,7 +8,16 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface CommitLogMapper {
 
+    /**
+     * 커밋 로그 저장
+     * @param commitLog 커밋 로그 정보
+     */
     void insert(CommitLog commitLog);
 
+    /**
+     * 특정 저장소의 커밋 로그 목록 조회
+     * @param repoId GitHub 저장소 ID
+     * @return 커밋 로그 목록
+     */
     List<CommitLog> findByRepoId(@Param("repoId") Long repoId);
 }
