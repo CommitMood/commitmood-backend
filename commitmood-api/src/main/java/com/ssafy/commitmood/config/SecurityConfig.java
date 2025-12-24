78ed046d1d41dc319d2705dc3dd7281ce5cab811
yeseong <yeseong31@naver.com>
2025-12-24 21:46:23 +0900

    🔨 fix(security): commitmood-api 테스트 시 SecurityConfig를 적용하지 않도록 수정

--- commitmood-api/src/main/java/com/ssafy/commitmood/config/SecurityConfig.java
+++ commitmood-api/src/main/java/com/ssafy/commitmood/config/SecurityConfig.java
@@ -9,9 +9,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +22,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!test & !testdata")
 @RequiredArgsConstructor
 public class SecurityConfig {
 
